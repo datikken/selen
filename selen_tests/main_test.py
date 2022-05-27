44f6979911546f4c4b20247fa920b2be04861a43
nshadybekov <nshadybekov@adsterra.com>
2022-05-27 23:07:48 +0300

    new visitor can start a list and retrieve it later

--- selen_tests/main_test.py
+++ selen_tests/main_test.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
+import unittest
 
-driver = webdriver.Chrome()
 
-driver.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-assert 'The install worked successfully! Congratulations!' in driver.title
+    def tearDown(self):
+        self.browser.quit()
 
-driver.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
