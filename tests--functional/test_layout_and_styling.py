654a40bdc2b9b5c0af3b0863911533895c5aac23
nshadybekov <shadybekov@cointelegraph.com>
2022-06-01 18:05:20 +0300

    tests splitted

---
+++ tests--functional/test_layout_and_styling.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from unittest import skip
+import unittest
+from .base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+    pass
