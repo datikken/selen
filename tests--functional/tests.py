2e9edef6ce02bf2c8b684bee9cb4fd84efbfc353
nshadybekov <shadybekov@cointelegraph.com>
2022-06-01 17:41:38 +0300

    test_cannot_add_empty_list_items added

--- tests--functional/tests.py
+++ tests--functional/tests.py
@@ -64,6 +64,22 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
